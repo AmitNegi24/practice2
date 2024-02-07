e92dad0273ed1c2d98357e64d7e0a3aa8761eddf
AmitNegi24 <mramitnegig1999@gmail.com>
2024-02-07 22:29:02 +0530

    commit

--- ECommerce/src/pages/Product/Product.jsx
+++ ECommerce/src/pages/Product/Product.jsx
@@ -1,23 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import '../Product/Product.css'
 import { useLocation } from 'react-router-dom'
+import { MdOutlineStar } from 'react-icons/md';
+
 function Product() {
-  const location=useLocation();
-  const [product,setProduct] = useState("");
-  useEffect(()=>{
-     setProduct(location.state.item.product)
-  },[])
+  const location = useLocation();
+  const [product, setProduct] = useState("");
+  useEffect(() => {
+    console.log(location.state.item.product)
+    setProduct(location.state.item.product)
+  }, [])
   return (
-    <div className='product'>
-      <div className='productimage'>
-          <img src={product.image} alt='productImage'/>
-          <p>${product.price}</p>
-          <button>Add to cart</button>
+    <div>
+      <div className='product'>
+        <div style={{ width: '30%', position: 'relative' }}>
+          <img className='productimage' src={product.image} alt='productimg' />
+        </div>
+        <div className='details'>
+          <div>
+            <h2>{product.title}</h2>
+            <div>
+              <p className='price'>${product.price}</p>
+            </div>
+          </div>
+          <div className='rating'>
+            <div style={{ color: 'tomato' }}>
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+            </div>
+            <p>(1 customer review)</p>
+          </div>
+          <p>{product.description}</p>
+          <div style={{display:'flex', gap:'4px'}}>
+            <div className='quantitybox'>
+              <p style={{ fontSize: '14px' }}>Quantity</p>
+              <div className='buttons'>
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+            </div>
+            <button className='addtocartbutton'>Add to cart</button>
+          </div>
+          <p>Category:{product.category}</p>
+        </div>
       </div>
-      <div className='details'>
-        details
-      </div>
+
     </div>
+
   )
 }
 
