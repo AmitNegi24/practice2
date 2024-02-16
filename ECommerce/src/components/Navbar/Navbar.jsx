34d257f1cb11550ffa6126b0e86bfa4ef5ebd1a0
AmitNegi24 <mramitnegig1999@gmail.com>
2024-02-16 21:42:35 +0530

    commit

--- ECommerce/src/components/Navbar/Navbar.jsx
+++ ECommerce/src/components/Navbar/Navbar.jsx
@@ -1,76 +1,85 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from "react-router-dom"
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt } from "react-icons/fa"
-import { getAuth, signOut } from "firebase/auth";
-import { ToastContainer, toast } from 'react-toastify'
-import logo from '../../assets/logo.png'
-import "../Navbar/Navbar.css"
-import { useDispatch, useSelector } from 'react-redux'
-import { removeUser } from '../../redux/ReducerSlice'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { getAuth, signOut } from 'firebase/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import logo from '../../assets/logo.png';
+import '../Navbar/Navbar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeUser, resetCart } from '../../redux/ReducerSlice';
 
 function Navbar() {
     const navigate = useNavigate();
     const productData = useSelector((state) => state.bazaar.productData);
-    const userInfo = useSelector((state) => state.bazaar.userInfo)
+    const userInfo = useSelector((state) => state.bazaar.userInfo);
     const dispatch = useDispatch();
     const [isOpen, setIsOpen] = useState(false);
+    
     const auth = getAuth();
 
     const logoutHandler = () => {
+        setIsOpen(false)
         signOut(auth)
             .then(() => {
                 dispatch(removeUser());
-                toast.success("Log out successfully");
+                dispatch(resetCart());
+                toast.success('Logged out successfully');
                 setTimeout(() => {
-                    navigate("/login")
-                }, 1500)
-            }).catch((err) => {
-                console.log(err)
+                    navigate('/login');
+                }, 1500);
             })
-    }
-    // console.log(userInfo);
+            .catch((err) => {
+                console.error('Error during logout:', err);
+                toast.error('Error logging out');
+            });
+    };
+
     return (
         <div>
-            <nav className='navbar'>
-                <div className='container'>
-                    <Link to='/'><img src={logo} alt='logo' /></Link>
-                    <div className='links'>
-                        <Link to={"/"}>Home</Link>
-                        <Link to={"/search"}><FaSearch /></Link>
-                        {userInfo && ( // Only render the cart link if userInfo exists (user is logged in)
-                            <Link to={"/cart"}>
+            <nav className="navbar">
+                <div className="container">
+                    <Link to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <div className="links">
+                        <Link to="/">Home</Link>
+                        <Link to="/search">
+                            <FaSearch />
+                        </Link>
+                        {userInfo && (
+                            <Link to="/cart">
                                 <FaShoppingBag />
                                 <span className="badge badge-warning">{productData.length}</span>
                             </Link>
                         )}
-
-                        {
-                            userInfo ? (
-                                <>
-                                    <img onClick={() => setIsOpen((prev) => !prev)} className='userlogo' src={userInfo ? userInfo.Image : "https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt='icon' />
-                                    <dialog className='dialog' open={isOpen}>
-                                        <div>
-                                            <button onClick={logoutHandler}>
-                                                <FaSignOutAlt />
-                                            </button>
-                                        </div>
-                                    </dialog>
-                                </>
-                            ) : (
-                                <>
-                                    <Link to={"/login"}><FaSignInAlt /></Link>
-                                </>
-                            )
-                        }
-                        {
-                            userInfo && <p>{userInfo.name}</p>
-                        }
+                        {userInfo ? (
+                            <>
+                                <img
+                                    onClick={() => setIsOpen((prev) => !prev)}
+                                    className="userlogo"
+                                    src={userInfo.Image || 'https://via.placeholder.com/150'}
+                                    alt="user icon"
+                                />
+                                <dialog className="dialog" open={isOpen}>
+                                    <div>
+                                        <button onClick={logoutHandler}>
+                                            <FaSignOutAlt />
+                                        </button>
+                                    </div>
+                                </dialog>
+                                <p>{userInfo.name}</p>
+                            </>
+                        ) : (
+                            <Link to="/login">
+                                <FaSignInAlt />
+                            </Link>
+                        )}
                     </div>
                 </div>
-
             </nav>
             <ToastContainer
-                position='top-left'
+                position="top-left"
                 autoClose={2000}
                 hideProgressBar={false}
                 newestOnTop={false}
@@ -79,10 +88,10 @@ function Navbar() {
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
-                theme='dark'
+                theme="dark"
             />
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
