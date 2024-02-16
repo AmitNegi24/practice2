fe4911ef1ed92ca1d2b8a72fad50c64e7161c400
AmitNegi24 <mramitnegig1999@gmail.com>
2024-02-16 14:00:41 +0530

    commit

--- ECommerce/src/pages/Cart/Cart.jsx
+++ ECommerce/src/pages/Cart/Cart.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CartItem from './CartItem'
 import './Cart.css'
+import { useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 function Cart() {
-  
+  const productData = useSelector((state) => state.bazaar.productData);
+  const [totalAmount, setTotalAmount] = useState("")
+  useEffect(() => {
+    let price = 0;
+    productData.map((item) => {
+      price += item.price * item.quantity;
+      return price;
+    })
+    setTotalAmount(price);
+  }, [productData])
   return (
     <div>
       <h1>Cart</h1>
       <div className='items'>
-        <CartItem/>
+        <CartItem />
         <div className='totals'>
           <div className='custom-container'>
             <h2 className='cartHeading'>Cart Total</h2>
             <p className='paragraph1'>
               SubTotal{" "}
               <span className='spantag'>
-                $200
+                ${totalAmount}
               </span>
             </p>
             <p className='paragraph1'>
@@ -27,7 +37,7 @@ function Cart() {
           </div>
           <p className='paragraph2'>
             {" "}
-            Total <span className='spantag'>$200</span>
+            Total <span className='spantag'>${totalAmount}</span>
           </p>
           <button className='btn'>CheckOut</button>
         </div>
--- ECommerce/src/redux/store.js
+++ ECommerce/src/redux/store.js
@@ -1,7 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
 import cartReducer from './reducerSlice.js'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 export const store = configureStore({
-    reducer: {
-        bazaar : cartReducer,
-    },
+    reducer: {bazaar:persistedReducer},
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+export let persistor = persistStore(store);
